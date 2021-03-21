f040a70cf48e663e2be7a7285c8e57314cb8f233
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2021-03-21 15:54:50 +0700

    Update insertDB.py

--- insertDB.py
+++ insertDB.py
@@ -10,16 +10,18 @@ mycol = mydb[collection]
 
 print("\n")
 
-name = input("Input Website Name: ")
-address = input("Input Website Address: ")
-username = input("Input Username: ")
-password = input("Input Password: ")
+inputColumn1 = input("Input Column1: ")
+inputColumn2 = input("Input Column2: ")
+inputColumn3 = input("Input Column3: ")
+inputColumn4 = input("Input Column4: ")
 
 mylist = [
-  { "name":name, "address":address, "username":username, "password":password}
+  { "column1Name":inputColumn1, "column2Name":inputColumn2, "column3Name":inputColumn3, "column41Name":inputColumn4}
 ]
 
 mycol.insert_many(mylist)
+
+print("Document has been inserted")
 
 print("\n")
 
@@ -27,20 +29,22 @@ continuing = input("Input Another Document? (y/n): ")
 
 while continuing == "y" :
   print("\n")
-  
-  name = input("Input Website Name: ")
-  address = input("Input Website Address: ")
-  username = input("Input Username: ")
-  password = input("Input Password: ")
+
+  inputColumn1 = input("Input Column1: ")
+  inputColumn2 = input("Input Column2: ")
+  inputColumn3 = input("Input Column3: ")
+  inputColumn4 = input("Input Column4: ")
 
   mylist = [
-    { "name":name, "address":address, "username":username, "password":password}
+    { "column1Name":inputColumn1, "column2Name":inputColumn2, "column3Name":inputColumn3, "column41Name":inputColumn4}
   ]
 
   mycol.insert_many(mylist)
+  
+  print("Document has been inserted")
 
   print("\n")
-  
+
   continuing = input("Input Another Document? (y/n): ")
 else :
   print("Thank You")
